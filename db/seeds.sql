2a4a9c09350869ae203729219f66024ef8f67cc3
Sarah Catherine Sefcik <sefciksa@gmail.com>
2018-12-15 07:59:26 -0500

    schema and connection

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgs (name) VALUES ('The Thunderbird');
+INSERT INTO burgs (name) VALUES ('The MacDaddy');
+INSERT INTO burgs (name, plain) VALUES ('The BIG Mac', true);
+INSERT INTO burgs (name, plain) VALUES ('The Whopper', true);
+INSERT INTO burgs (name, plain) VALUES ('The Storm Trooper', true);
+INSERT INTO burgs (name) VALUES ('The Cattle Call');
