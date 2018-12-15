b094a8ce0fc356a24113baa7978ad6ba0ab4bd9c
Sarah Catherine Sefcik <sefciksa@gmail.com>
2018-12-15 07:01:38 -0500

    server.js

--- server.js
+++ server.js
@@ -3,3 +3,24 @@ var express = require("express");
 var PORT = process.env.PORT || 8080;
 
 var app = express();
+
+// Serve static
+app.use(express.static("public"));
+
+// Parse app
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+// Handlebars
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes
+var routes = require("./controllers/burgerController.js");
+
+app.use(routes);
+
+// Start server
+
+// log when the server has started
