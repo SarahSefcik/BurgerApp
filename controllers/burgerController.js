794e6510a60d728b0465ab8aed45d9fdef3a19ae
Sarah Catherine Sefcik <sefciksa@gmail.com>
2018-12-15 07:23:47 -0500

    cont get post

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -3,7 +3,28 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model
+var burg = require("../models/burger.js");
 
 // Create all routes and logic within
+router.get("/", function(req, res) { 
+  burg.all(function(data) { 
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burgs", function(req, res) {
+  burg.create([
+    "name", "plain"
+  ], [
+    req.body.name, req.body.plain
+  ], function(result) { 
+    res.json({ id: result.insertId });
+  });
+});
+
 
 // Export routes for server.js
