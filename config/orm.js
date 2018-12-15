e2e405ce064f40b23219d0b863def2218bf493d6
Sarah Catherine Sefcik <sefciksa@gmail.com>
2018-12-15 07:43:37 -0500

    orm.js

---
+++ config/orm.js
@@ -0,0 +1,15 @@
+// Import MYSQL Connection
+
+// Helper function for SQL syntax
+
+// Helper function for converting object key pairs to SQL
+
+// loop through keys
+
+// skip hidden props
+
+// single comma-separated string
+
+// object for all SQL stmnt functions
+
+// Export orm object for model
